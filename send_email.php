5218d861558335affa45b000d06a4193c76ebc63
ilyesjarray <jarrayilyes18@gmail.com>
2025-02-28 22:45:46 +0100

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -2,16 +2,17 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
+    $phone = htmlspecialchars($_POST['phone']);
     $message = htmlspecialchars($_POST['message']);
-    
+
     $to = "jarrayilyes18@gmail.com"; // Replace with your email address
     $subject = "New message from $name";
     $headers = "From: $email\r\n" .
                "Reply-To: $email\r\n" .
                "X-Mailer: PHP/" . phpversion();
-    
-    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
-    
+
+    $body = "Name: $name\nEmail: $email\nPhone: $phone\n\nMessage:\n$message";
+
     if (mail($to, $subject, $body, $headers)) {
         echo "Email sent successfully!";
     } else {
