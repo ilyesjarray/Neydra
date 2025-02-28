9ce1876308dcac776c4697a87999c7f3fef3411f
ilyesjarray <jarrayilyes18@gmail.com>
2025-02-28 20:38:58 +0100

    Add files via upload

---
+++ send_email.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+    
+    $to = "jarrayilyes18@gmail.com"; // Replace with your email address
+    $subject = "New message from $name";
+    $headers = "From: $email\r\n" .
+               "Reply-To: $email\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+    
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+    
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Failed to send email.";
+    }
+}
+?>
